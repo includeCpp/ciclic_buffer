72487a51e1d20e67007527c18dc6b4994ca72b28
includeCpp <anton.a.v.1987@gmail.com>
2022-08-01 20:14:42 +0300

    Initialize empty repository

---
+++ ciclic_buffer.hpp
@@ -0,0 +1,4 @@
+#ifndef __CICLIC_BUFFER_HPP__
+#define __CICLIC_BUFFER_HPP__
+
+#endif __CICLIC_BUFFER_HPP__
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"ciclic_buffer.hpp"
+
+int main(){
+
+
+
+
+	return 0;
+} //main
