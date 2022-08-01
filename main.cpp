f5a8e0a4cea0ae033cd0774d3f83e6dd7136832e
includeCpp <anton.a.v.1987@gmail.com>
2022-08-01 21:23:48 +0300

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 #include"ciclic_buffer.hpp"
 
-int main(){
+template<typename T>
+bool check_equal(const T& buff_1, const T& buff_2);
 
+int main(){
+	array_impl::ciclic_buffer<int> buff_1;
+	list_impl::ciclic_buffer<int> buff_2;
+
+	for(int i = 0; i < 50; i++){
+		buff_1.push_back(i + (i * 2));
+		buff_2.push_back(i + (i * 2));
+
+		buff_1.push_front(i - (i * 2));
+		buff_2.push_front(i - (i * 2));
+	}
+
+	if(check_equal(buff_1, buff_2)){
+		std::cout << "Both buffers are equal." << std::endl;
+	} else {
+		std::cout << "Both buffers aren't equal." << std::endl;
+		return 0;
+	}
+
+	for(int i = 0; i < 25; i++){
+		buff_1.pop_back(i + (i * 2));
+		buff_2.pop_back(i + (i * 2));
+
+		buff_1.pop_front(i - (i * 2));
+		buff_2.pop_front(i - (i * 2));
+	}
+
+	if(check_equal(buff_1, buff_2)){
+		std::cout << "Both buffers are equal." << std::endl;
+	} else {
+		std::cout << "Both buffers aren't equal." << std::endl;
+		return 0;
+	}
 
 
 
